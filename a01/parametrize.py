564c042a2ad1268e1b7a1ffe63fd5c510a41eb7c
Dreamyay <2413837673@qq.com>
2018-09-30 10:08:40 +0800

    001ya

---
+++ a01/parametrize.py
@@ -0,0 +1,9 @@
+import pytest
+class Test():
+    # @pytest.mark.parametrize("name",["张","李"])
+    # def test01(self,name):
+    #     print("name",name)
+    @pytest.mark.parametrize("name,age",[("康",18),("黄",26),("李",20)])
+    def test01(self,name,age):
+        print("name：",name,"age：",age)
+
