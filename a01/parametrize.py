a1a6440c38161f2557a48426be992472000f5189
Dreamyay <43697725+Dreamyay@users.noreply.github.com>
2018-09-30 12:57:20 +0800

    Update parametrize.py

--- a01/parametrize.py
+++ a01/parametrize.py
@@ -3,7 +3,7 @@ class Test():
     # @pytest.mark.parametrize("name",["张","李"])
     # def test01(self,name):
     #     print("name",name)
-    @pytest.mark.parametrize("name,age",[("康",18),("黄",26),("李",20)])
+    @pytest.mark.parametrize("name,age",[("康",18),("黄",26),("王",20)])
     def test01(self,name,age):
         print("name：",name,"age：",age)
 
